44cc9ff1f4465087542092631532563b11f45bf9
geniusmerely <dima.tlens@gmail.com>
2024-03-14 01:58:18 +0200

    add json types

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,11 @@
 pub mod atom;
 pub use atom::Atom;
+
+pub type Type = u32;
+pub const ANY: Type = 0;
+pub const STRING: Type = 1;
+pub const ARRAY: Type = 2;
+pub const OBJECT: Type = 4;
+pub const BOOLEAN: Type = 8;
+pub const NUMBER: Type = 16;
+pub const NULL: Type = 32;
