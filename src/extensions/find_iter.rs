2294ade03bb21586888d0494b3d1920f69018a4a
geniusmerely <dima.tlens@gmail.com>
2024-03-14 15:54:02 +0200

    fix extension find_iter

--- src/extensions/find_iter.rs
+++ src/extensions/find_iter.rs
@@ -1,6 +1,6 @@
-use crate::Atom;
-
 use super::{find::FindEx as _, rfind::RFindEx as _, DefaultEx as _};
+use crate::error::Result;
+use crate::Atom;
 
 #[derive(Default)]
 pub struct FindIter<'a> {
@@ -8,6 +8,22 @@ pub struct FindIter<'a> {
     atom_back: Option<Atom<'a>>,
     value_type: crate::Type,
     needle: String,
+}
+impl<'a> FindIter<'a> {
+    pub fn new(
+        atom: impl Into<Option<Atom<'a>>>,
+        needle: impl Into<String>,
+        value_type: crate::Type,
+    ) -> Self {
+        let atom_next = atom.into();
+        let atom_back = atom_next.end();
+        Self {
+            atom_next,
+            atom_back,
+            value_type,
+            needle: needle.into(),
+        }
+    }
 }
 
 impl<'a> Iterator for FindIter<'a> {
@@ -41,21 +57,34 @@ impl<'a> DoubleEndedIterator for FindIter<'a> {
 pub trait FindIterEx<'a> {
     fn find_iter(
         &self,
-        needle: impl Into<String>,
+        needle: impl AsRef<str>,
         value_type: crate::Type,
     ) -> impl Iterator<Item = Atom<'a>>;
 }
 impl<'a> FindIterEx<'a> for Atom<'a> {
     fn find_iter(
         &self,
-        needle: impl Into<String>,
+        needle: impl AsRef<str>,
         value_type: crate::Type,
     ) -> impl Iterator<Item = Atom<'a>> {
-        FindIter {
-            atom_back: self.end(),
-            atom_next: Some(self.clone()),
-            value_type,
-            needle: needle.into(),
-        }
+        FindIter::new(self.clone(), needle.as_ref(), value_type)
+    }
+}
+impl<'a> FindIterEx<'a> for Result<Atom<'a>> {
+    fn find_iter(
+        &self,
+        needle: impl AsRef<str>,
+        value_type: crate::Type,
+    ) -> impl Iterator<Item = Atom<'a>> {
+        FindIter::new(self.clone().ok(), needle.as_ref(), value_type)
+    }
+}
+impl<'a> FindIterEx<'a> for Option<Atom<'a>> {
+    fn find_iter(
+        &self,
+        needle: impl AsRef<str>,
+        value_type: crate::Type,
+    ) -> impl Iterator<Item = Atom<'a>> {
+        FindIter::new(self.clone(), needle.as_ref(), value_type)
     }
 }
